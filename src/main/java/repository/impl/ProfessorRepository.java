d3e818c176abe4052a29904fecff107ac113114b
AminSafari207 <safarin207368@gmail.com>
2025-09-02 12:04:19 +0330

    Implement ProfessorRepository class

---
+++ src/main/java/repository/impl/ProfessorRepository.java
@@ -0,0 +1,20 @@
+package repository.impl;
+
+import com.jsum.model.person.Professor;
+import jakarta.persistence.EntityManager;
+import repository.base.BaseRepository;
+
+import java.util.List;
+
+public class ProfessorRepository extends BaseRepository<Professor, Long> {
+    public ProfessorRepository(EntityManager em) {
+        super(em, Professor.class);
+    }
+
+    public List<Professor> findByIds(List<Long> ids) {
+        if (ids == null || ids.isEmpty()) return List.of();
+        return em.createQuery("select p from " + classRef.getSimpleName() + " p where p.id in :ids", classRef)
+                .setParameter("ids", ids)
+                .getResultList();
+    }
+}
