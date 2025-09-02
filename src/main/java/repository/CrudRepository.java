2f7fc440f6a692749187be4bdae8d7a9ccbea67c
AminSafari207 <safarin207368@gmail.com>
2025-09-02 11:55:16 +0330

    Implement BaseRepository abstract class

--- src/main/java/repository/CrudRepository.java
+++ src/main/java/repository/CrudRepository.java
@@ -7,5 +7,5 @@ public interface CrudRepository<T, ID> {
     T save(T entity);
     Optional<T> findById(ID id);
     List<T> findAll();
-    void deleteById(ID id);
+    boolean deleteById(ID id) throws Exception;
 }
---
+++ src/main/java/repository/base/BaseRepository.java
@@ -0,0 +1,52 @@
+package repository.base;
+
+import com.jsum.model.base.BaseEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public abstract class BaseRepository<T, ID> extends BaseEntity<ID> implements CrudRepository<T, ID> {
+    private final EntityManager em;
+    private final Class<T> classRef;
+
+    public BaseRepository(EntityManager em, Class<T> classRef) {
+        this.em = em;
+        this.classRef = classRef;
+    }
+
+    @Override
+    public T save(T entity) {
+        return em.merge(entity);
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return Optional.ofNullable(em.find(classRef, id));
+    }
+
+    @Override
+    public List<T> findAll() {
+        return em.createQuery("select e from " + classRef.getSimpleName() + " e", classRef).getResultList();
+
+    }
+
+    @Override
+    public boolean deleteById(ID id) throws Exception {
+        try {
+            T ref = em.getReference(classRef, id);
+            em.remove(ref);
+            return true;
+        } catch (EntityNotFoundException e) {
+            return false;
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+
+    protected Class<T> getClassRef() {
+        return classRef;
+    }
+}
