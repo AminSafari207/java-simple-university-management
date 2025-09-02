2b4de14eb62171491a13672a6783a30650f666d5
AminSafari207 <safarin207368@gmail.com>
2025-09-02 15:22:09 +0330

    Add isActive and gpa fields to Student entity model

--- src/main/java/com/jsum/model/person/Student.java
+++ src/main/java/com/jsum/model/person/Student.java
@@ -3,6 +3,7 @@ package com.jsum.model.person;
 import com.jsum.model.base.Person;
 import com.jsum.model.Course;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Transient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +14,26 @@ public class Student extends Person<Student> {
 
     private List<Course> enrolledCourses = new ArrayList<>();
 
+    @Transient
+    private boolean isActive;
+
+    @Transient
+    private Double gpa;
+
     public String getMajor() {
         return major;
     }
 
     public List<Course> getEnrolledCourses() {
         return enrolledCourses;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public Double getGpa() {
+        return gpa;
     }
 
     public Student major(String major) {
@@ -28,6 +43,16 @@ public class Student extends Person<Student> {
 
     public Student enrolledCourses(List<Course> enrolledCourses) {
         this.enrolledCourses = enrolledCourses;
+        return this;
+    }
+
+    public Student isActive(boolean isActive) {
+        this.isActive = isActive;
+        return this;
+    }
+
+    public Student gpa(Double gpa) {
+        this.gpa = gpa;
         return this;
     }
 
